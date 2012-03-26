93aa0a5c0b383dcc49011e75c720c87d0a2b14d5
Berrouz at Luxoft <berrouz@gmail.com>
2012-03-26 18:28:59 +0000

    optimized solutions is ready

--- fun-box.ru/find_missed_optimized.rb
+++ fun-box.ru/find_missed_optimized.rb
@@ -1,4 +1,4 @@
-﻿require 'rspec'
+require 'rspec'
 require 'benchmark'
 class Array
   def	find_two_missed
@@ -10,37 +10,31 @@ class Array
 			puts temp.first.size
       break if temp.first.adjacent? && temp.size == 1
     end
-    return temp.first.return_which_is_missed if temp.size ==1
+    return temp.first.return_which_are_missed if temp.size ==1
     temp.map(&:find_missed)
   end
 
   def mid_position
-    size%2 == 0 ? (size/2-1) : ((size+1)/2)-1
+    @size%2 == 0 ? (size/2-1) : ((size+1)/2)-1
   end
 
   def shorten
     case get_shift
-    when 0
-      [self[mid_position,size-mid_position]]
-    when 1
-      [self[0, mid_position+1], self[mid_position,size-mid_position+1]]
-    when 2
-      [self[0, mid_position+1]]
+    when 0 then [self[mid_position,size-mid_position]]
+    when 1 then [self[0, mid_position+1], self[mid_position,size-mid_position+1]]
+    when 2 then [self[0, mid_position+1]]
     end
   end
 
   def get_shift
     case self[mid_position]
-    when mid_position+first
-      0
-    when mid_position+1+first
-      1
-    when mid_position+2+first
-      2
+    when mid_position+first   then 0
+    when mid_position+1+first then 1
+    when mid_position+2+first then 2
     end
   end
 
-  def return_which_is_missed
+  def return_which_are_missed
     (first+1..last-1).to_a
   end
 
@@ -54,7 +48,7 @@ class Array
       temp = temp.slice_array
     end
     temp.return_which_is_missed
-	end
+  end
 
   def	slice_array
     case self[mid_position]
@@ -78,40 +72,40 @@ Benchmark.bm(2) do |r|
 end
 describe Array do
 
-	it "should find middle positions" do
-		[1,2,3,4,6,7,8,10,11,12].mid_position.should == 4
-	end
+  it "should find middle positions" do
+    [1,2,3,4,6,7,8,10,11,12].mid_position.should == 4
+  end
 
-	it "should find where 2 digits" do
-		[1,2,3,4,5,6,7,8,10,11,12].get_shift.should == 0
-		[1,2,3,4,6,7,8,10,11,12].get_shift.should == 1
- 		[1,2,4,6,7,8,9,10,11,12].get_shift.should == 2
-	end
+  it "should find where 2 digits" do
+    [1,2,3,4,5,6,7,8,10,11,12].get_shift.should == 0
+    [1,2,3,4,6,7,8,10,11,12].get_shift.should == 1
+    [1,2,4,6,7,8,9,10,11,12].get_shift.should == 2
+  end
 
-	it "should return 2 arrays " do
-		[1,2,3,4,6,7,8,10,11,12].shorten.should == [[1,2,3,4,6], [6,7,8,10,11,12]]
-		[1,2,4,6,7,8,9,10,11,12].shorten.should == [[1,2,4,6,7]]
-		[1,2,4,6,7].shorten.should == [[1,2,4],[4,6,7]]
-		[1,2,3,5,6,7,9,10].shorten.should == [[1,2,3,5],[5,6,7,9,10]]
-		[1,2,3,4,5,6,7,9].shorten.should == [[4,5,6,7,9]]
-		[1,2,3,4,5,6,8,10].shorten.should == [[4,5,6,8,10]]
-	end
+  it "should return 2 arrays " do
+    [1,2,3,4,6,7,8,10,11,12].shorten.should == [[1,2,3,4,6], [6,7,8,10,11,12]]
+    [1,2,4,6,7,8,9,10,11,12].shorten.should == [[1,2,4,6,7]]
+    [1,2,4,6,7].shorten.should == [[1,2,4],[4,6,7]]
+    [1,2,3,5,6,7,9,10].shorten.should == [[1,2,3,5],[5,6,7,9,10]]
+    [1,2,3,4,5,6,7,9].shorten.should == [[4,5,6,7,9]]
+    [1,2,3,4,5,6,8,10].shorten.should == [[4,5,6,8,10]]
+  end
 
-	it "should return missed from array" do
-		[1,3].return_which_is_missed.should == [2]
-	end
+  it "should return missed from array" do
+    [1,3].return_which_are_missed.should == [2]
+  end
 
-	it	"should find 2 neighbor digits around missed in array" do
-		[1,2,4,5,6].slice_array.should == [1,2,4]
-		[1,2,4].slice_array.should == [2,4]
-	end
+  it "should find 2 neighbor digits around missed in array" do
+    [1,2,4,5,6].slice_array.should == [1,2,4]
+    [1,2,4].slice_array.should == [2,4]
+  end
 
-	it "should return missed digit" do
+  it "should return missed digit" do
     [1,2,4,5,6].find_missed.should == [3]
-		[1,2,3,4,5,6,7,8,9,10,11,12,13,15].find_missed.should == [14]
-	end
+    [1,2,3,4,5,6,7,8,9,10,11,12,13,15].find_missed.should == [14]
+  end
 
-	it "should find 2 missed digits" do
-		[1,2,4,6,7,8,9,10,11,12].find_two_missed.should == [[3],[5]]
-	end
+  it "should find 2 missed digits" do
+    [1,2,4,6,7,8,9,10,11,12].find_two_missed.should == [[3],[5]]
+  end
 end
